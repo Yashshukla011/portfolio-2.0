adeb08439c1373dd53b40d89b1d41509ac7848b9
yash shukla <yashshuklashuklas6@gmail.com>
2026-08-01 11:04:54 +0530

    wdfg

--- Backend/controller/contact.js
+++ Backend/controller/contact.js
@@ -1,93 +1,59 @@
-import Contact from "../Model/contact.model.js";
-import nodemailer from "nodemailer";
+export const sendContact = async (req, res) => {
+  try {
+    console.log("1. Request received");
 
+    const { name, email, subject, message } = req.body;
 
+    console.log("2. Data received");
 
-export const sendContact = async(req,res)=>{
+    await Contact.create({
+      name,
+      email,
+      subject,
+      message,
+    });
 
-    try{
+    console.log("3. MongoDB Saved");
 
-        const {
-            name,
-            email,
-            subject,
-            message
-        } = req.body;
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
+    console.log("4. Transport Created");
 
+    await transporter.verify();
 
-        const contact = await Contact.create({
+    console.log("5. SMTP Verified");
 
-            name,
-            email,
-            subject,
-            message
+    await transporter.sendMail({
+      from: email,
+      to: process.env.EMAIL_USER,
+      subject: `Portfolio Contact: ${subject}`,
+      html: `
+        <h3>New Message</h3>
+        <p>Name: ${name}</p>
+        <p>Email: ${email}</p>
+        <p>Message: ${message}</p>
+      `,
+    });
 
-        });
+    console.log("6. Email Sent");
 
+    return res.status(200).json({
+      success: true,
+      message: "Message Sent",
+    });
 
+  } catch (error) {
+    console.log("ERROR:", error);
 
-        // Email setup
-console.log("EMAIL:", process.env.EMAIL_USER);
-console.log("PASS:", process.env.EMAIL_PASS);
-        const transporter =
-        nodemailer.createTransport({
-
-            service:"gmail",
-
-            auth:{
-                user:process.env.EMAIL_USER,
-                pass:process.env.EMAIL_PASS
-            }
-
-        });
-
-
-
-        await transporter.sendMail({
-
-            from:email,
-
-            to:process.env.EMAIL_USER,
-
-            subject:`Portfolio Contact: ${subject}`,
-
-            html:`
-
-            <h3>New Message</h3>
-
-            <p><b>Name:</b>${name}</p>
-
-            <p><b>Email:</b>${email}</p>
-
-            <p><b>Message:</b>${message}</p>
-
-            `
-
-        });
-
-
-
-        res.status(201).json({
-
-            success:true,
-
-            message:"Message sent successfully"
-
-        });
-
-
-
-    }catch(error){
-
-        res.status(500).json({
-
-            success:false,
-
-            message:error.message
-
-        });
-
-    }
-
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
