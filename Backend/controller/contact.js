5fd327cf603094b029cb0f53038c84887ea7f3b0
yash shukla <yashshuklashuklas6@gmail.com>
2026-08-02 18:44:16 +0530

    Add Google verification file

--- Backend/controller/contact.js
+++ Backend/controller/contact.js
@@ -32,17 +32,16 @@ export const sendContact = async (req, res) => {
 
     console.log("5. SMTP Verified");
 
-   await transporter.sendMail({
-  from: process.env.EMAIL_USER,
-  replyTo: email,
-  to: process.env.EMAIL_USER,
-  subject: `Portfolio Contact: ${subject}`,
-  html: `
-    <h3>New Message</h3>
-    <p><b>Name:</b> ${name}</p>
-    <p><b>Email:</b> ${email}</p>
-    <p><b>Message:</b> ${message}</p>
-  `,
+ const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  family: 4, // Force IPv4
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
     console.log("6. Email Sent");
