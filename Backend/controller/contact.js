99930055c0254f502096e80db730e49c874265bd
yash shukla <yashshuklashuklas6@gmail.com>
2026-08-02 18:40:02 +0530

    Add Google verification file

--- Backend/controller/contact.js
+++ Backend/controller/contact.js
@@ -32,17 +32,18 @@ export const sendContact = async (req, res) => {
 
     console.log("5. SMTP Verified");
 
-    await transporter.sendMail({
-      from: email,
-      to: process.env.EMAIL_USER,
-      subject: `Portfolio Contact: ${subject}`,
-      html: `
-        <h3>New Message</h3>
-        <p>Name: ${name}</p>
-        <p>Email: ${email}</p>
-        <p>Message: ${message}</p>
-      `,
-    });
+   await transporter.sendMail({
+  from: process.env.EMAIL_USER,
+  replyTo: email,
+  to: process.env.EMAIL_USER,
+  subject: `Portfolio Contact: ${subject}`,
+  html: `
+    <h3>New Message</h3>
+    <p><b>Name:</b> ${name}</p>
+    <p><b>Email:</b> ${email}</p>
+    <p><b>Message:</b> ${message}</p>
+  `,
+});
 
     console.log("6. Email Sent");
 
